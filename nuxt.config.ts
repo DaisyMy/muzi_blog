32f369cdf57c577f436242bd342acea73cc80a82
daisy <8931428+daisyyou@user.noreply.gitee.com>
2025-01-20 22:55:20 +0800

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,13 +4,17 @@ export default defineNuxtConfig({
   devtools: { enabled: false },
   app: {
     baseURL: '/muzi_blog',
+    // buildAssetsDir: 'nuxt_assets',
     head: {
       title: 'muzi_blog',
     },
   },
-  nitro: {
-    output: {
-      publicDir: path.join(__dirname, 'docs'),
-    },
-  },
+  // nitro: {
+  //   output: {
+  //     publicDir: path.join(__dirname, 'docs'),
+  //   },
+  // },
+  // experimental: {
+  //   payloadExtraction: false,
+  // },
 });
